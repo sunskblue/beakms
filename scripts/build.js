a7479e847ada42bf46b25e1e5c1246a1f86573a7
starkwang <wsd312@163.com>
2020-06-18 21:45:56 +0800

    feat: GENERATE_SOURCEMAP = false

--- scripts/build.js
+++ scripts/build.js
@@ -11,6 +11,8 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+process.env.GENERATE_SOURCEMAP = false;
+
 const figEnv = process.argv.slice(2)[0];
 process.env.FIG_ENV = figEnv || 'prod';
 let isFigEnv = ['prod', 'pre', 'test', 'dev'].includes(figEnv);
